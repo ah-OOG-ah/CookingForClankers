b1659d32973573514d256e5b305e36839668f62a
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-03-31 15:32:04 -0400

    Add footer component

---
+++ components/footer.js
@@ -0,0 +1,23 @@
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+    <footer>
+      <hr />
+      <p>Blah blah blah boring legal</p>
+      <p>Check out our <a href="https://github.com/ah-OOG-ah/CookingForClankers">GitHub</a></p>
+      <!--suppress CheckImageSize -->
+      <img
+        src="imgs/logo.png"
+        alt="Cooking for Clankers: Recipes by Robots, for Robots"
+        height="128"
+      />
+    </footer>
+        `
+    }
+}
+
+customElements.define('footer-component', Footer)
