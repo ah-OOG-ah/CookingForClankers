e0cb88da8f6b3a758603584e31f568eb697f1404
Jonah Ebent <jonahaaronebent@gmail.com>
2026-04-08 16:00:46 -0400

    ignore silly errors

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ host=csc391-vm1.eng.franciscan.edu
 remote=$user@$host
 projectDir=/srv/csc391web/team3/project
 scp -r components css imgs js *.html README.md $remote:$projectDir
-ssh $remote "chgrp -R csc391team3 $projectDir"
+ssh $remote "chgrp -R csc391team3 $projectDir 2> /dev/null"
