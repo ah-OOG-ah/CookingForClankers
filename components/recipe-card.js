109ae62e5c2f45f8c7313abba4e65b3a6a6b43a4
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-04-13 13:33:24 -0400

    Link recipe cards back to recipe page

--- components/recipe-card.js
+++ components/recipe-card.js
@@ -11,42 +11,21 @@ class RecipeCard extends HTMLElement {
     const name = this.getAttribute("name") || "Loading...";
     const imgSrc = this.getAttribute("img-src");
     const story = this.getAttribute("story") || "Loading...";
-    // ...
 
-    /* TODO make it horizontal (this is Bootstrap's example)
-<div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="..." class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-</div>
-
-*/
-
-    this.innerHTML = /* html */ `
+    /* TODO make it horizontal */
+    this.innerHTML = `
         <article class="card">
           <div class="position-absolute top-0 end-0 m-2">
             <i class="favorite-btn" role="button" data-recipe-id=${id}></i>
           </div>
-          <img
-            src=${imgSrc}
-            class="card-img"
-          />
+          <img src=${imgSrc} class="card-img" alt="..."/>
 
           <div class="card-body">
             <h5 class="card-title">${name}</h5>
             <p class="card-text">${story}</p>
+            <a href="recipe.html?id=${id}" class="stretched-link"></a>
           </div>
-        </article>
-        `;
+        </article>`;
   }
 }
 
