0756b0dd13b8ddf9506ba1eabebe7f72002252f5
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-04-12 22:40:25 -0400

    Link the index cards

--- js/recipeIndex.js
+++ js/recipeIndex.js
@@ -1,24 +1,29 @@
 import { ALL_RECIPES, fetchAllRecipes, recipeImage } from "./recipe_data.js";
 
+/**
+ * @param {Recipe} recipe
+ * @return {HTMLDivElement} A recipe card for the given recipe.
+ */
 function indexCard(recipe) {
-  return `
-<div class="row g-0">
-  <div class="col-md-4">
-    <img src="${recipeImage(recipe)}" class="img-fluid rounded-start" alt="...">
-  </div>
-  <div class="col-md-8">
-    <div class="card-body">
-      <h5 class="card-title">${recipe.name}</h5>
-      <p class="card-text">This is a sample description, as we don't have a better one generated.</p>
-    </div>
-  </div>
-</div>`;
+  let newElem = document.createElement("div");
+  newElem.className = "card mb-3";
+  newElem.innerHTML = `
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="${recipeImage(recipe)}" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${recipe.name}</h5>
+          <p class="card-text">This is a sample description, as we don't have a better one generated.</p>
+          <a href="recipe.html?id=${recipe.id}" class="stretched-link"></a>
+        </div>
+      </div>
+    </div>`;
+  return newElem;
 }
 
 await fetchAllRecipes();
 for (const recipe of ALL_RECIPES.values()) {
-  let newElem = document.createElement("div");
-  newElem.className = "card mb-3";
-  newElem.innerHTML = indexCard(recipe);
-  document.getElementById("mainColumn").appendChild(newElem);
+  document.getElementById("mainColumn").appendChild(indexCard(recipe));
 }
