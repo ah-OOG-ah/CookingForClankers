2efe1a9c7751a0b7cb6806849319c5a4ca70fe3d
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-04-12 21:35:25 -0400

    Update recipeIndex.js

--- js/recipeIndex.js
+++ js/recipeIndex.js
@@ -9,8 +9,7 @@ function indexCard(recipe) {
   <div class="col-md-8">
     <div class="card-body">
       <h5 class="card-title">${recipe.name}</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      <p class="card-text">This is a sample description, as we don't have a better one generated.</p>
     </div>
   </div>
 </div>`;
