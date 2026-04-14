96a20ff2473fa7fd5abc15354c783a0c493263ab
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-04-14 09:36:04 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -euo pipefail
+
 user=$1
 host=csc391-vm1.eng.franciscan.edu
 remote=$user@$host
 projectDir=/srv/csc391web/team3/project
-scp -r components css imgs js *.html README.md $remote:$projectDir
-ssh $remote "chgrp -R csc391team3 $projectDir 2> /dev/null"
+
+npm run sass
+npm run fmt
+scp -r components css imgs js ./*.html README.md node_modules data "$remote":$projectDir
+# shellcheck disable=SC2029
+ssh "$remote" \
+ "chgrp -R csc391team3 $projectDir && find $projectDir -type d -exec chmod 775 {} \; && find $projectDir -type f -exec chmod 664 {} \;"
