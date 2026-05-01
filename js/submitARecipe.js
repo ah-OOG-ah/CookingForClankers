bff7e9cc23a13af85f46cf85d4412fcd273df720
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-05-01 08:03:18 -0400

    Partial form

---
+++ js/submitARecipe.js
@@ -0,0 +1,60 @@
+let nextId = 0xffff;
+let ingId = 1;
+
+/** @type HTMLInputElement */
+const nextAmt = document.getElementById("newIngAmt");
+/** @type HTMLInputElement */
+const nextIng = document.getElementById("newIngredient");
+
+function addIngredient() {
+  const id = ingId++;
+  const row = document.createElement("div");
+  row.className = "row mb-2";
+  row.id = `ingRow${id}`;
+
+  const inputGroup = document.createElement("div");
+  inputGroup.className = "col-sm-9 input-group";
+  row.appendChild(inputGroup);
+
+  const amtLabel = document.createElement("label");
+  amtLabel.className = "visually-hidden";
+  amtLabel.htmlFor = `ing${id}amt`;
+  amtLabel.innerText = "Amount";
+  inputGroup.appendChild(amtLabel);
+
+  const amtInput = document.createElement("input");
+  amtInput.type = "number";
+  amtInput.className = "form-control";
+  amtInput.name = `ing${id}amt`;
+  amtInput.id = `ing${id}amt`;
+  amtInput.valueAsNumber = nextAmt.valueAsNumber;
+  inputGroup.appendChild(amtInput);
+
+  const ingLabel = document.createElement("label");
+  ingLabel.className = "visually-hidden";
+  ingLabel.htmlFor = `ing${id}`;
+  ingLabel.innerText = `Ingredient ${id}`;
+  inputGroup.appendChild(ingLabel);
+
+  const ingInput = document.createElement("input");
+  ingInput.type = "te ";
+  ingInput.className = "w-50 form-control";
+  ingInput.name = `ing${id}`;
+  ingInput.id = `ing${id}`;
+  ingInput.value = nextIng.value;
+  inputGroup.appendChild(ingInput);
+
+  const removeBtn = document.createElement("button");
+  removeBtn.className = "btn btn-secondary";
+  removeBtn.innerText = "REMOVE";
+  removeBtn.type = "button";
+  removeBtn.onclick = () => removeIngredient(id);
+  inputGroup.appendChild(removeBtn);
+
+  document.getElementById("ingredients").appendChild(row);
+}
+
+/** @param {number} id */
+function removeIngredient(id) {
+  document.getElementById(`ingRow${id}`).remove();
+}
